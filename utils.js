2688af6a5849bfecc04871656727d27ebe20ca54
vru4447123 <vvru062@gmail.com>
2026-03-17 20:21:26 +0300

    utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
-const COIN_EMOJI = "🪙";
+const COIN_EMOJI = "<:Coin:1483370941583982612>";
 
 function formatCoins(amount) {
-  return `🪙 **${amount.toLocaleString()} LEN**`;
+  return `<:Coin:1483370941583982612> **${amount.toLocaleString()} LEN**`;
 }
 
 module.exports = { COIN_EMOJI, formatCoins };
